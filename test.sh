12f9ab2f1939bbddb8aeffefb69d4605cde1c071
Matas Kaminskas <matas.kaminskas@protonmail.com>
2023-01-23 12:27:20 +0200

    upd: test script

--- test.sh
+++ test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-files=("text.txt") 
+files=("text.txt") # "bigfile.txt" "alice29.txt" "beer.jpg" "pineapple.jpg") 
 
 for file in "${files[@]}"
 do
   for i in {2..16}
   do
     python huffman.py -c "$file" $i >/dev/null
-    python huffman.py -d "$file".huf >/dev/null
+    python huffman.py -d "$file" >/dev/null
     if ! cmp -s "$file" output; then
         echo "Compression and decompression with bit length $i failed on $file"
     fi
