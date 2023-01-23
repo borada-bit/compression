85d3770a91e2e1bcb5069adeb5077cd4e3c5a42f
Matas Kaminskas <matas.kaminskas@protonmail.com>
2023-01-23 14:37:17 +0200

    upd: final, decompression little faster

--- io_stream.py
+++ io_stream.py
@@ -2,8 +2,9 @@ from bitarray import bitarray
 from collections import defaultdict
 import pickle
 from pprint import pprint
+import time
 
-CHUNK_SIZE = 1024*1024*30 # megabytes
+CHUNK_SIZE = 1#1024*1024*30 # megabytes
 
 # read file for the first time and return frequencies and padding
 def get_frequencies_from_file(input_file: str, bit_len: int):
@@ -29,7 +30,7 @@ def get_frequencies_from_file(input_file: str, bit_len: int):
             if len(bytes) < bytes_to_read and (len(bytes) * 8) % bit_len != 0:
                 padded_bits = [0] * (bit_len - (len(bits) % bit_len))
                 bits.extend(padded_bits)
-
+            
             # convert all bits to long string for optimisation
             bits_str = bits.to01()
             # start = time.time()
@@ -67,8 +68,8 @@ def write_hf_code_to_file(input_file, output_file, hf_code, bit_len):
                 chunk = bits_str[i:i+bit_len]
                 encoded.extend(hf_code[chunk])
             
-            with open(output_file, 'ab') as of:
-                encoded.tofile(of)
+        with open(output_file, 'ab') as of:
+            encoded.tofile(of)
     pass
 
             
@@ -77,7 +78,7 @@ def decode_file(file_name):
     with open(f'{file_name}.hf', 'rb') as f:
         # read header -> huffman code table
         header = pickle.load(f)
-        pprint(header)
+        # pprint(header)
         bit_len = header['bit_len']
         if header['padded_bits']:
             print(f"{header['padded_bits']=}")
@@ -91,7 +92,8 @@ def decode_file(file_name):
         assert(bytes_to_read*8 % bit_len == 0)
 
         original_bits = bitarray()
-        
+        start = bitarray()
+        start_t = time.time()
         while True:
             bytes = f.read(bytes_to_read)
             if not bytes:
@@ -99,28 +101,27 @@ def decode_file(file_name):
 
             bits = bitarray()
             bits.extend(start)
+            # clear this one so only new data is extended every loop
+            start.clear()
             # retrieve encoded bits
             bits.frombytes(bytes)
+            # convert bits to string for optimisation
+            bits_str = bits.to01()
 
-            value = bitarray()
-            for bit in bits:
-                # probably not efficient
-                value.extend([bit])
-                # print(f"Currently have {value}")
-                original_value = header.get(value.to01())
-                # print(f'Received original value {original_value}')
+            value = ""
+            for bit in bits_str:
+                value += bit
+                original_value = header.get(value)
                 if original_value and symbol_translated_count < symbol_count:
                     symbol_translated_count += 1
-                    value.clear()
+                    value = ""
                     original_bits.extend(original_value)
-            
-            start = value
-        # Only remove if this is the last chunk?
+            start.extend([int(b) for b in value])
+        print(f"Time taken to decompress {time.time() - start_t}")
         if header['padded_bits']:
             # remove padded bits
             original_bits = original_bits[:-header['padded_bits']]
-        print(f'Decompressed file chunk in bits:\n{original_bits}')
 
-        with open('output', 'wb') as of:
-            original_bits.tofile(of)
+    with open('output', 'wb') as of:
+        original_bits.tofile(of)
     pass
